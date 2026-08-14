e95cbfe2d91dbebd83be2c30c0bed51daf96b6f7
Harshil Patel <165708484+MastermindCore@users.noreply.github.com>
2026-08-14 16:14:52 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,27 @@
+const fixtures = [
+  { match: 1, teamA: "LLC", teamB: "Nyeri Good Sheperd", time: "-", result: "5:1" },
+  { match: 2, teamA: "Xord", teamB: "Sigma Bot", time: "-", result: "3:2" },
+  { match: 3, teamA: "RoboDynamos", teamB: "404 Not Found", time: "-", result: "5:0" },
+  { match: 4, teamA: "BenSuperStrikers", teamB: "Elprocessor", time: "-", result: "7:0" },
+  { match: 5, teamA: "solterra", teamB: "vision IOT", time: "-", result: "7:1" },
+  { match: 6, teamA: "Scorpy Bots", teamB: "Flence", time: "-", result: "7:2" },
+];
+
+const teams = [
+  { name: "RoboDynamo", school: "-", members: ["Ethan"] },
+  { name: "404 Not Found", school: "S.C.L.P Samaj", members: ["Juhil"] },
+  { name: "LLC", school: "Linux", members: ["Alan"] },
+  { name: "-", school: "Nyeri good Shepherd", members: ["Mark"] },
+  { name: "BenZSuperStriker", school: "S.C.L.P Samaj", members: ["Rudra", "Arya","Vedansh"] },
+  { name: "Flence", school: "-", members: ["Elkana","Amon","Denis"] },
+  { name: "Xord", school: "-", members: ["Kb"] },
+  { name: "Orbitron", school: "-", members: ["David"] },
+  { name: "Solterra", school: "-", members: ["Mano"] },
+  { name: "ElProcessor", school: "-", members: ["Omar"] },
+  { name: "-", school: "S.C.L.P Samaj", members: ["Christian"] },
+  { name: "Botnaldo", school: "-", members: ["Shem","William"] },
+  { name: "Scorpy Bot", school: "-", members: ["Ian"] },
+  { name: "Sigma Bot", school: "-", members: ["Fred"] },
+  { name: "willary", school: "-", members: ["hillary"] },
+  { name: "Vision IOT", school: "-", members: ["francis"] }
+];
